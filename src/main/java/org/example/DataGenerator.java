d653b2526109169ff21f2a1c3b462a7bfd1b87c0
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-12 03:58:39 +0300

    Добавил недостающие проверки

--- src/main/java/org/example/DataGenerator.java
+++ src/main/java/org/example/DataGenerator.java
@@ -1,4 +1,16 @@
 package org.example;
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class DataGenerator {
+    public static String getRandomString() {
+        return RandomStringUtils.randomAlphabetic(5);
+    }
+    public static String getRandomEmail() {
+        return (RandomStringUtils.randomAlphabetic(5) + "@yandex.ru");
+    }
+
+    public static String getRandomStringPhone() {
+        return RandomStringUtils.randomNumeric(10);
+    }
 
 }
--- src/test/java/org/example/TestPatchUser.java
+++ src/test/java/org/example/TestPatchUser.java
@@ -1,4 +1,58 @@
 package org.example;
-public class TestPatchUser {
 
+import io.qameta.allure.Description;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPatchUser {
+    private User user;
+    private UserClient userClient;
+    private String accessToken;
+
+    @Before
+    public void setUp() {
+        user = UserGenerator.getUser();
+        userClient = new UserClient();
+    }
+
+    @After
+    @Description("Удаление пользователя")
+    public void deleteUser() {
+        ValidatableResponse response = userClient.delete(accessToken);
+        Assert.assertEquals(202, response.extract().statusCode());
+    }
+
+    @Test
+    @Description("Успешное изменение пользователя")
+    public void patchUser() {
+        ValidatableResponse response = userClient.create(user);
+        Assert.assertEquals(200, response.extract().statusCode());
+        ValidatableResponse login = userClient.login(UserCredentials.from(user));
+        accessToken = response.extract().path("accessToken");
+        Assert.assertEquals(200, login.extract().statusCode());
+        user = UserGenerator.getUser();
+        ValidatableResponse patch = userClient.patch(user, accessToken);
+        Assert.assertNotEquals((patch.extract().path("user.email")),
+                response.extract().path("user.email"));
+        Assert.assertEquals(200, patch.extract().statusCode());
+
+    }
+
+    @Test
+    @Description("Изменение  пользователя без авторизации")
+    public void pathUserWithoutAuthorization() {
+        ValidatableResponse response = userClient.create(user);
+        Assert.assertEquals(200, response.extract().statusCode());
+        accessToken = "";
+        ValidatableResponse patch = userClient.patch(user, accessToken);
+        assertEquals("You should be authorised",
+                patch.extract().path("message"));
+        Assert.assertEquals(401, patch.extract().statusCode());
+        accessToken = response.extract().path("accessToken");
+    }
 }
